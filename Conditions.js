7a91217284fa95e7d938586d1fda563e71fbfbfe
LeonMa2001 <leonma271@hotmail.com>
2024-10-09 04:22:31 +1100

    Fixed issues reguarding map, also created new divs to prepare for storing new bar charts

--- Conditions.js
+++ Conditions.js
@@ -1,12 +1,12 @@
 // Path to your external JSON file
-const jsonFile = 'Healthsites.json';
+const jsonFile = 'Conditions.json';
 
 // Fetch the Vega-Lite specification from the external JSON file
 fetch(jsonFile)
     .then(response => response.json())
     .then(spec => {
-        // Embed the fetched Vega-Lite specification into the div with id 'HealthsitesMap'
-        vegaEmbed('#HealthsitesMap', spec).then(function(result) {
+        // Embed the fetched Vega-Lite specification into the div with id 'ConditionsChart'
+        vegaEmbed('#ConditionsChart', spec).then(function(result) {
             console.log("Map rendered successfully");
         }).catch(console.error);
     })
